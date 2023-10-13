1c301762aea70e985994f46239a2385c3c73e48d
josephchigiz <josephochego@gmail.com>
2023-10-13 18:31:20 +0530

    push Task 8

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+const myObject = {
+    type: 'object',
+    value: 12
+};
+
+function incr() {
+    this.value++;
+}
+myObject.incr = incr;
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
