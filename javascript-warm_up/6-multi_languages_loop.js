2693dcdddebfe7e4ba590e75576b46cd1ae394be
josephchigiz <josephochego@gmail.com>
2023-10-13 07:52:09 +0530

    push Task 2

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,10 @@
-let myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
-console.log(myVar[0]);
-console.log(myVar[1]);
-console.log(myVar[2]);
+const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < myVar.length; i++) {
+    console.log(myVar[i]);
+}
+
+// let myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
+// console.log(myVar[0]);
+// console.log(myVar[1]);
+// console.log(myVar[2]);
