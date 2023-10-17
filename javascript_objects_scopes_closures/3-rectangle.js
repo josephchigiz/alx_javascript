f4dcd9dd78602fd1909d9014a8aaf176200e3cc9
josephchigiz <josephochego@gmail.com>
2023-10-17 18:44:40 +0530

    push Task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,11 +4,20 @@ class Rectangle {
         if (w > 0 && h > 0) {
             this.width = w;
             this.height = h;
-            this.print = function (x) {
-                console.log(`${this.width} ${this.height}`);
-            };
         };
     }
+
+    print = function (x) {
+        if (this.width && this.height) {
+            for (let i = 0; i < this.height; i++) {
+                let row = "";
+                for (let j = 0; j < this.width; j++) {
+                    row += "X";
+                }
+                console.log(row);
+            }
+        }
+    };
 }
 
 module.exports = Rectangle;
