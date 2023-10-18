5d39ba0ebaef50640baf13fc01609b0b46a6f165
josephchigiz <josephochego@gmail.com>
2023-10-18 13:35:00 +0530

    push task 3

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,26 +1,52 @@
+// #!/usr/bin/node
+// const request = require('request');
+
+// const url = process.argv[2];
+// request.get(url, function(error, response, body) {
+//     if (error) {
+//         console.error(`Error: ${error}`);
+//         return;
+//     }
+//     if (response.statusCode !== 200) {
+//         console.error(`Status: ${response.statusCode}`);
+//         return;
+//     }
+//     // this script prints No. of movies he was in
+//     const films= JSON.parse(body).results;
+//     const id = "18";
+//     let count = 0;
+
+//     //Iterate film by film
+//     for (const film of films) {
+//         if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+//         count++;
+//         }
+//     }
+//     console.log(count)
+// });
+
+
 #!/usr/bin/node
 const request = require('request');
 
 const url = process.argv[2];
 request.get(url, function(error, response, body) {
-    if (error) {
-        console.error(`Error: ${error}`);
-        return;
-    }
-    if (response.statusCode !== 200) {
-        console.error(`Status: ${response.statusCode}`);
-        return;
-    }
-    // this script prints No. of movies he was in
-    const films= JSON.parse(body).results;
-    const id = "18";
-    let count = 0;
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Status: ${response.statusCode}`);
+    return;
+  }
+  const films= JSON.parse(body).results;
+  const id = "18";
+  let count = 0;
 
-    //Iterate film by film
-    for (const film of films) {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
-        count++;
-        }
+  for (const film of films) {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+      count++;
     }
-    console.log(count)
+  }
+  console.log(count)
 });
