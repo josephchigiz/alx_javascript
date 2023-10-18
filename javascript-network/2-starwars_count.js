83f0f1622328333442b8129b0c9abe2f7fd83aec
josephchigiz <josephochego@gmail.com>
2023-10-19 00:47:18 +0530

    push task 3

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,6 +1,35 @@
+// const request = require('request');
+
+// const url = process.argv[2];
+// request.get(url, function(error, response, body) {
+//     if (error) {
+//         console.error(`Error: ${error}`);
+//         return;
+//     }
+//     if (response.statusCode !== 200) {
+//         console.error(`Status: ${response.statusCode}`);
+//         return;
+//     }
+//     // this script prints No. of movies he was in
+//     // const films= JSON.parse(body).results;
+//     const films= JSON.parse(body).results;
+//     const id = "18";
+//     let count = 0;
+
+//     //Iterate film by film
+//     for (const film of films) {
+//         if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+//         count++;
+//         }
+//     }
+//     console.log(count)
+// });
+
 const request = require('request');
 
 const url = process.argv[2];
+const id = "18";
+
 request.get(url, function(error, response, body) {
     if (error) {
         console.error(`Error: ${error}`);
@@ -12,15 +41,15 @@ request.get(url, function(error, response, body) {
     }
     // this script prints No. of movies he was in
     // const films= JSON.parse(body).results;
-    const films= JSON.parse(body);
-    const charID = "18";
+    const films= JSON.parse(body).results;
     let count = 0;
 
     //Iterate film by film
-    for (const film of films.results) {
-        if (film.characters.includes(charID)) {
+    for (const film of films) {
+        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
         count++;
         }
     }
     console.log(count)
 });
+
