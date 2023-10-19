574a28abdea105909d5ebb04162c516bc7402736
josephchigiz <josephochego@gmail.com>
2023-10-19 22:50:36 +0530

    push task 2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -25,12 +25,44 @@
 //     console.log(count)
 // });
 
+
+
+
+// const request = require('request');
+
+// const url = process.argv[2];
+// const id = "18";
+
+// request.get(url, function(error, response, body) {
+//     if (error) {
+//         console.error(`Error: ${error}`);
+//         return;
+//     }
+//     if (response.statusCode !== 200) {
+//         console.error(`Status: ${response.statusCode}`);
+//         return;
+//     }
+//     // this script prints No. of movies he was in
+//     // const films= JSON.parse(body).results;
+//     const films= JSON.parse(body).results;
+//     let count = 0;
+
+//     //Iterate film by film
+//     for (const film of films) {
+//         if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+//         count++;
+//         }
+//     }
+//     console.log(count)
+// });
+
 const request = require('request');
 
-const url = process.argv[2];
-const id = "18";
+const apiUrl = process.argv[2]; // The API URL for Star Wars films
+const characterId = "18"; // Character ID for Wedge Antilles
 
-request.get(url, function(error, response, body) {
+// Make a GET request to the Star Wars API films URL
+request.get(apiUrl, function(error, response, body) {
     if (error) {
         console.error(`Error: ${error}`);
         return;
@@ -39,17 +71,17 @@ request.get(url, function(error, response, body) {
         console.error(`Status: ${response.statusCode}`);
         return;
     }
-    // this script prints No. of movies he was in
-    // const films= JSON.parse(body).results;
-    const films= JSON.parse(body).results;
+
+    const films = JSON.parse(body).results;
     let count = 0;
 
-    //Iterate film by film
+    // Iterate through the films and count those where Wedge Antilles is present
     for (const film of films) {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
-        count++;
+        // Check if characters array contains the character ID
+        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+            count++;
         }
     }
-    console.log(count)
-});
 
+    console.log(`Number of movies where Wedge Antilles is present: ${count}`);
+});
