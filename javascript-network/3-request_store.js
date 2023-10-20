1ab08169a84ee289260f24b4d5737d79c1b39382
josephchigiz <josephochego@gmail.com>
2023-10-20 07:33:56 +0530

    push task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const url = process.argv[2];
-const fileName = 'loremipsum.html';
+const fileName = process.argv[3];
 
 const filePath = path.join(__dirname, fileName);
 request.get(url).pipe(fs.createWriteStream(filePath));
