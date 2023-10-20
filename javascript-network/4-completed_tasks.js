47850e4af6961446485c3fe23097dda16570e0e2
josephchigiz <josephochego@gmail.com>
2023-10-20 07:54:08 +0530

    push task 4

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    if (response.statusCode !== 200) {
+        console.error(response.statusCode);
+        return;
+    }
+
+    const toDoTasks = JSON.parse(body);
+
+    const completedTasks = new Map;
+
+    toDoTasks.forEach((todo) => {
+        if (todo.completed) {
+            if (completedTasks.has(todo.userId)) {
+                completedTasks.set(todo.userId, completedTasks.get(todo.userId) + 1);
+            } else {
+                completedTasks.set(todo.userId, 1);
+            }
+        }
+    });
+
+    completedTasks.forEach((completedCount, userId) => {
+        console.log(`${userId}: ${completedCount}`);
+    });
+});
