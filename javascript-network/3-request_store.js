13fd52f667643c2c3a16ff8f23261f187cc21f68
josephchigiz <josephochego@gmail.com>
2023-10-20 07:38:27 +0530

    push task 3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,3 +1,8 @@
+#!/usr/bin/node
+/**
+ * This script gets the contents of a webpage and stores it in a file.
+ */
+
 const request = require('request');
 const fs = require('fs');
 const path = require('path');
